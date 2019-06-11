f49e4888da7a7b318fdc7d0423a946f043033559
clyde grey <clyde.grey@gmail.com>
2019-06-11 00:26:17 -0400

    sticky sidebar added

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import unsplash from '../api/unsplash';
 import ImageList from './ImageList/ImageList';
 import SearchBar from './SearchBar';
 import Header from './Header/Header';
 import Footer from './Footer/Footer';
-import UIContainer from './UIContainer/UIContainer';
+import styles from './App.module.css';
+// import UIContainer from './UIContainer/UIContainer';
 
 const App = () => {
-  const [appState, appSetState] = useState({ images: [] });
+  let searchBarRef = useRef(null);
+  let imageListRef = useRef(null);
+  let innerRef = useRef(null);
 
+  const [appState, appSetState] = useState({ images: [] });
+  const [stickyState, stickySetState] = useState('');
+  const getTop = el => el.current.getBoundingClientRect().top;
+  const getBottom = el => {
+    console.log(el);
+    console.log(el.current.getBoundingClientRect().top);
+    console.log(el.current.clientHeight);
+
+    return el.current.getBoundingClientRect().top + el.current.clientHeight;
+  };
+
+  const getHeight = el => el.current.clientHeight;
   const onFormSubmit = async term => {
     const res = await unsplash.get('/search/photos', {
       params: { query: term }
@@ -17,6 +32,56 @@ const App = () => {
     if (res.status === 200) {
       appSetState({ images: res.data.results });
     }
+  };
+
+  const stickyComponent = () => {
+    let fixedTop, fixedBottom;
+    const searchBarTop = getTop(searchBarRef);
+    const searchBarBottom = getBottom(searchBarRef);
+    const imageListTop = getTop(imageListRef);
+    const imageListBottom = getBottom(imageListRef);
+    const innerHeight = getHeight(innerRef);
+    console.log(searchBarTop);
+    console.log(searchBarBottom);
+    console.log(imageListBottom);
+    console.log(innerHeight);
+    console.log(searchBarTop <= 0);
+    console.log(searchBarBottom >= 0);
+    console.log(searchBarBottom > innerHeight);
+    if (
+      searchBarTop <= 0 &&
+      searchBarBottom >= 0 &&
+      searchBarBottom > innerHeight
+    ) {
+      stickySetState('top');
+      console.log('top');
+    } else if (searchBarTop <= 0 && searchBarBottom <= innerHeight) {
+      stickySetState('bottom');
+      console.log('bottom');
+    } else {
+      stickySetState('');
+      console.log('neither');
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', stickyComponent);
+    console.log('Created');
+    return () => {
+      console.log('Cleaned up');
+      window.removeEventListener('scroll', stickyComponent);
+    };
+  }, []);
+
+  const getStickyStyle = () => {
+    console.log(stickyState);
+    let stickyStyle = { position: 'static' };
+    if (stickyState === 'top') {
+      stickyStyle = { position: 'fixed', top: '0' };
+    } else if (stickyState === 'bottom') {
+      stickyStyle = { position: 'absolute', bottom: '0' };
+    }
+    return stickyStyle;
   };
 
   // const getImagesMarkup() {
@@ -28,10 +93,21 @@ const App = () => {
   return (
     <div>
       <Header />
-      <UIContainer
-        aside={<SearchBar onFormSubmit={onFormSubmit} />}
-        section={<ImageList images={appState.images} />}
-      />
+      <main className={styles.main}>
+        <aside>
+          <div className={styles.inner} ref={searchBarRef}>
+            <div ref={innerRef} style={getStickyStyle()}>
+              <SearchBar onFormSubmit={onFormSubmit} />
+            </div>
+          </div>
+        </aside>
+        <section>
+          <div ref={imageListRef}>
+            <ImageList images={appState.images} />
+          </div>
+        </section>
+      </main>
+
       <Footer />
     </div>
   );
